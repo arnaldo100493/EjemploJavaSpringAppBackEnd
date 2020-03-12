b691a5ffe51343c8ce4237cb42d1f74b55a6179c
Arnaldo Barrios <abarrime@everis.com>
2020-03-12 17:17:09 -0500

    Modificación Configuaración Spring y creación de unidad de persistencia.

--- EjemploJavaSpringAppBackEnd/src/main/java/com/ejemploangularapp/main/EjemploJavaSpringAppBackEndApplication.java
+++ EjemploJavaSpringAppBackEnd/src/main/java/com/ejemploangularapp/main/EjemploJavaSpringAppBackEndApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EjemploJavaSpringAppBackEndApplication {
-    
-    public EjemploJavaSpringAppBackEndApplication(){
-        
+
+    public EjemploJavaSpringAppBackEndApplication() {
+
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(EjemploJavaSpringAppBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EjemploJavaSpringAppBackEndApplication.class, args);
+    }
 
 }
